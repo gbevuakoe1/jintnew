7fc0473c08b22df8fb032bf5c2bef06048225138
gbevuakoe1 <emmaaddaedoe@gmail.com>
2025-10-09 21:15:37 -0400

    Add files via upload

---
+++ CSS/script.js
@@ -0,0 +1,35 @@
+const openBtn = document.getElementById("open-btn");
+const closeBtn = document.getElementById("close-btn");
+const links = document.querySelector(".nav-wrapper");
+
+openBtn.addEventListener("click", () =>{
+    links.classList.add("menu-open");
+});
+closeBtn.addEventListener("click", () =>{
+    links.classList.remove("menu-open")
+});
+
+
+window.addEventListener("scroll", () => {
+    navbar = document.getElementById("navbar");
+
+    if(window.scrollY > 100){
+        navbar.classList.add("nav-fixed");
+    }
+    else{
+        navbar.classList.remove("nav-fixed")
+    }
+});
+
+    // fixed nav bar
+
+window.addEventListener("scroll", () => {
+    navbar = document.getElementById("navbar");
+
+    if(window.scrollY > 100){
+        navbar.classList.add("fixed-nav");
+    }
+    else{
+        navbar.classList.remove("fixed-nav")
+    }
+});
