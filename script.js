11f68fddda764111a9794a7fc7b1d257bff744d9
gbevuakoe1 <emmaaddaedoe@gmail.com>
2025-11-21 07:02:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ openBtn.addEventListener("click", () =>{
 closeBtn.addEventListener("click", () =>{
     links.classList.remove("menu-open")
 });
+
+
+
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(document.querySelectorAll('.slide'));
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let index = 0;
+
+function updateCarousel() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    track.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+    index = (index + 1) % slides.length;
+    updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+    index = (index - 1 + slides.length) % slides.length;
+    updateCarousel();
+});
+
+// Auto-slide every 3 seconds
+setInterval(() => {
+    index = (index + 1) % slides.length;
+    updateCarousel();
+}, 3000);
+
+window.addEventListener('resize', updateCarousel);
+
+
